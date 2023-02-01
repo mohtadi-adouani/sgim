1e58e3b6e833e703942c4d040675b7782076bb80
hddembele <papendembele@gmail.com>
2023-02-01 15:39:31 +0100

    Ajout de la ligne debug URL

--- src/app.ts
+++ src/app.ts
@@ -1,6 +1,11 @@
 const express = require('express')
 let app = express(); // création de l'objet représentant notre application express
-let port = 8001;
+let port = 8080;
+
+app.use((req, res, next)=> {
+    console.log('URL : ' + req.url)
+    next()
+})
 
 app.get('/', function(req, res) { // création de la route sous le verbe get
     res.send('Hello world  ! ') // envoi de hello world a l'utilisateur
@@ -8,5 +13,5 @@ app.get('/', function(req, res) { // création de la route sous le verbe get
 
 
 app.listen(port, () =>  { // ecoute du serveur sur le port 8080
-    console.log('le serveur fonctionne')
+    console.log('Started on port 8080')
 })
