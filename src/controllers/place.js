683cd46154a494c7c48aa0cd3e0c9e5b9de97b00
Mohtadi Adouani <madouani@sol-solution.com>
2023-02-19 17:44:38 +0100

    Ajout du controller place

---
+++ src/controllers/place.js
@@ -0,0 +1,36 @@
+const User = require('../models').User
+const Place = require('../models').Place
+const { Sequelize } = require('sequelize');
+module.exports = {
+
+    // get all users
+    getAllPlaces: ( req, res ) => {
+
+        Place.findAll( {
+            limit: 5
+        }).then(places => {
+            return res.status(200).json({
+                places
+            })
+        }).catch(err => {
+            return res.status(400).json({err})
+        })
+    },
+        // get all users
+        getOnePlace: ( req, res ) => {
+
+            Place.findOne( {
+                where: { id: req.params.id },
+            }).then(place => {
+                return res.status(200).json({
+                    place
+                })
+            }).catch(err => {
+                return res.status(400).json({err})
+            })
+        },
+
+
+
+
+}
