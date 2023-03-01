0f6d80694445b8de903f527252177cfd024f2c8f
Mohtadi Adouani <madouani@sol-solution.com>
2023-03-01 22:42:37 +0100

    Add middleware auth.

--- src/utils/jwt.utils.js
+++ src/utils/jwt.utils.js
@@ -12,5 +12,24 @@ module.exports = {
             {
                 expiresIn: "2h",
             });
+    },
+    verifyToken : function(req, res, next){
+        const token =
+            req.body.token || req.query.token || req.headers["x-access-token"];
+
+        if (!token) {
+            return res.status(403).send("A token is required for authentication");
+        }
+        try {
+            const decoded = jwt.verify(token, JWT_SIGN_SECRET);
+            req.user = decoded;
+        } catch (err) {
+            return res.status(401).send("Invalid Token");
+        }
+        return next();
     }
 }
+
+
+
+module.exports = verifyToken;
