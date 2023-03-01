e7a6d07ef535c07a801aa4c51f73b37c7509327b
Mohtadi Adouani <madouani@sol-solution.com>
2023-03-01 12:15:33 +0100

    Model Tag created

---
+++ src/models/tag.js
@@ -0,0 +1,41 @@
+'use strict';
+const { Sequelize } = require('sequelize');
+const {
+  Model
+} = require('sequelize');
+module.exports = (sequelize, Sequelize) => {
+  const Tag = sequelize.define('Tag',{
+      id: {
+          allowNull: false,
+          primaryKey: true,
+          type: Sequelize.UUID
+      },
+      name: {
+          type: Sequelize.STRING
+      },
+      createdAt: {
+          allowNull: false,
+          type: Sequelize.DATE
+      },
+      updatedAt: {
+          allowNull: false,
+          type: Sequelize.DATE
+      },
+      // association places
+      PlaceId: {
+          type: Sequelize.UUID,
+      },
+      // association objects
+      ObjectId: {
+          type: Sequelize.UUID,
+      }
+  });
+
+  Tag.associate = function (models) {
+      Tag.belongsToMany(models.Place, {through: 'TagsPlaces'});
+      // Tag.belongsToMany(models.Place, {through: 'TagsPlaces', foreignKey: 'PlaceId'});
+      // Tag.belongsToMany(models.Object, {through: 'TagsObjects', foreignKey: 'ObjectId'});
+      Tag.belongsToMany(models.Object, {through: 'TagsObjects'});
+    };
+  return Tag;
+};
