74c0e0d73179a744d9313461e0e4ad91633d8cad
Mohtadi Adouani <madouani@sol-solution.com>
2023-03-02 12:40:00 +0100

    Remove useless element

--- src/models/index.js
+++ src/models/index.js
@@ -5,7 +5,6 @@ const path = require('path');
 const Sequelize = require('sequelize');
 const process = require('process');
 const basename = path.basename(__filename);
-//const env = process.env.NODE_ENV || 'development';
 const config = require(__dirname + '/../../config/config');
 const db = {};
 
