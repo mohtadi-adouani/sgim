472c31bace3b84aea76710631bb2f89446d0b829
Mohtadi Adouani <madouani@sol-solution.com>
2023-03-02 13:16:12 +0100

    Create 2 different files to middleware and token generation.

--- src/utils/jwt.utils.js
+++ src/utils/jwt.utils.js
@@ -1,6 +1,6 @@
 const jwt = require("jsonwebtoken");
-
-const JWT_SIGN_SECRET = 'kqdjqklsdiuoeuoaizuzu';
+// config env
+const config = require('../../config/config');
 
 module.exports = {
     generateTokenForUser : function (user) {
@@ -8,25 +8,10 @@ module.exports = {
             userId : user.id,
             isAdmin : user.isAdmin
         },
-            JWT_SIGN_SECRET,
+            config.JWT_SIGN_SECRET,
             {
                 expiresIn: "2h",
             });
-    },
-    verifyToken : function(req, res, next){
-        const token =
-            req.body.token || req.query.token || req.headers["x-access-token"];
-
-        if (!token) {
-            return res.status(403).send("A token is required for authentication");
-        }
-        try {
-            const decoded = jwt.verify(token, JWT_SIGN_SECRET);
-            req.user = decoded;
-        } catch (err) {
-            return res.status(401).send("Invalid Token");
-        }
-        return next();
     }
 }
 
