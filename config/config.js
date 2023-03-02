ebf025c60f01f2660362c09df82d7e9f786f7dd8
Mohtadi Adouani <madouani@sol-solution.com>
2023-03-02 13:14:57 +0100

    add jws secret key to sign tokens to settings

--- config/config.js
+++ config/config.js
@@ -5,34 +5,46 @@ var environments = {};
 environments.development = {
     'api_port': 3000,
     'envName': 'development',
+
     // database env
     "username": "admin",
     "password": "admin",
     "database": "sgim_db_dev",
     "host": "127.0.0.1",
-    "dialect": "postgres"
+    "dialect": "postgres",
+
+    // jws secret key to sign tokens
+    'JWT_SIGN_SECRET' : 'cnzkejlze456r4zzre5r4ze4rze4rz6e4r'
 };
 
 environments.production = {
     'api_port': 5000,
     'envName': 'production',
+
     // database env
     "username": "admin",
     "password": "admin",
     "database": "sgim_db_prod",
     "host": "127.0.0.1",
-    "dialect": "postgres"
+    "dialect": "postgres",
+
+    // jws secret key to sign tokens
+    'JWT_SIGN_SECRET' : 'cnzkejlze456r4zzre5r4ze4rze4rz6e4r'
 };
 
 environments.test = {
     'api_port': 4000,
     'envName': 'test',
+
     // database env
     "username": "admin",
     "password": "admin",
     "database": "sgim_db_test",
     "host": "127.0.0.1",
-    "dialect": "postgres"
+    "dialect": "postgres",
+
+    // jws secret key to sign tokens
+    'JWT_SIGN_SECRET' : 'cnzkejlze456r4zzre5r4ze4rze4rz6e4r'
 };
 
 var currEnvironment = typeof (process.env.NODE_ENV) == 'string' ? process.env.NODE_ENV : 'development';
