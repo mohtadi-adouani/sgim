a9d144c5116daa0ede96de7cae5eeaf499ae5a9c
Mohtadi Adouani <madouani@sol-solution.com>
2023-03-02 12:21:15 +0100

    Add api port on config file and import it

--- src/app.ts
+++ src/app.ts
@@ -7,10 +7,12 @@ const place = require('./routes/place')
 const object = require('./routes/object')
 const tag = require('./routes/tag')
 
+// config env
+const config = require('../config/config');
+
 
 // APP
 let app = express(); // création de l'objet représentant notre application express
-let port = 8002;
 app.use(express.json())
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
@@ -34,6 +36,6 @@ app.use('/api', object)
 // Routing tags
 app.use('/api', tag)
 
-app.listen(port, () =>  { // ecoute du serveur sur le port 8001
-    console.log('Started on port ' + port)
+app.listen(config.api_port, () =>  { // ecoute du serveur sur le port 8001
+    console.log('Started on port ' + config.api_port)
 })
