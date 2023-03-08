0f73a4b599729787ad73865038dea29df9aef1b9
Mohtadi Adouani <madouani@sol-solution.com>
2023-03-08 12:10:20 +0100

    Fix auth

--- src/middleware/auth.js
+++ src/middleware/auth.js
@@ -5,18 +5,18 @@ const config = require('../../config/config');
 
 module.exports = {
     verifyToken : function(req, res, next){
-        // const token =
-        //     req.body.token || req.query.token || req.headers["x-access-token"];
-        //
-        // if (!token) {
-        //     return res.status(403).send("A token is required for authentication");
-        // }
-        // try {
-        //     const decoded = jwt.verify(token, config.JWT_SIGN_SECRET);
-        //     req.user = decoded;
-        // } catch (err) {
-        //     return res.status(401).send("Invalid Token");
-        // }
+        const token =
+            req.body.token || req.query.token || req.headers["x-access-token"];
+
+        if (!token) {
+            return res.status(403).send("A token is required for authentication");
+        }
+        try {
+            const decoded = jwt.verify(token, config.JWT_SIGN_SECRET);
+            req.user = decoded;
+        } catch (err) {
+            return res.status(401).send("Invalid Token");
+        }
         return next();
     }
 }
