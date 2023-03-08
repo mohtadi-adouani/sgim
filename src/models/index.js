fae7ce4ab7701368645f46120c63de232b8e5031
Mohtadi Adouani <madouani@sol-solution.com>
2023-03-08 22:35:19 +0100

    move syn

--- src/models/index.js
+++ src/models/index.js
@@ -37,8 +37,7 @@ Object.keys(db).forEach(modelName => {
     db[modelName].associate(db);
   }
 });
-
+sequelize.sync();
 db.sequelize = sequelize;
 db.Sequelize = Sequelize;
-sequelize.sync();
 module.exports = db;
