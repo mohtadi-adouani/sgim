faeef94be08b9c2ed214bd8d313ee963db7f908d
Mohtadi Adouani <madouani@sol-solution.com>
2023-03-08 12:24:27 +0100

    Add authorisation

--- src/middleware/auth.js
+++ src/middleware/auth.js
@@ -18,6 +18,27 @@ module.exports = {
             return res.status(401).send("Invalid Token");
         }
         return next();
-    }
+    },
+    // auth_read_user : async function (req, res, next) {
+    //     const user_id = req.user.userId;
+    //     const wanted_user_id = req.params.id;
+    //     if(!(user_id && wanted_user_id) ){
+    //         return res.status(400).send("All params required.");
+    //     }
+    //     let req_user = await User.findByPk(wanted_user_id);
+    //     let wanted_user = await User.findByPk(user_id).then(wanted_user => {
+    //             if( req_user.isAdmin ||  )
+    //     }).catch(error => {
+    //         return res.status(404).send("User not found");
+    //     })
+    //
+    //     try {
+    //         const decoded = jwt.verify(token, config.JWT_SIGN_SECRET);
+    //         req.user = decoded;
+    //     } catch (err) {
+    //         return res.status(401).send("Invalid Token");
+    //     }
+    //     return next();
+    // }
 }
 
