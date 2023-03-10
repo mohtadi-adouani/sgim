2ceb3343c316b2f5b831788c11af3679e2b99008
Mohtadi Adouani <madouani@sol-solution.com>
2023-03-10 13:33:46 +0100

    update config file

--- config/config.js
+++ config/config.js
@@ -1,56 +1,48 @@
-// Container for environments
-var environments = {};
+require('dotenv').config();
 
-// environments
-environments.development = {
-    'api_port': 3000,
-    'envName': 'development',
+module.exports = {
 
-    // database env
-    "username": "admin",
-    "password": "admin",
-    "database": "sgim_db_dev",
-    "host": "localhost",
-    "dialect": "postgres",
+    "development" : {
+        'api_port': 3000,
+        'envName': 'development',
+        // database env
+        "username": "admin",
+        "password": "admin",
+        "database": "sgim_db_dev",
+        "host": "sgim_db",
+        "dialect": "postgres",
 
-    // jws secret key to sign tokens
-    'JWT_SIGN_SECRET' : 'cnzkejlze456r4zzre5r4ze4rze4rz6e4r'
-};
+        // jws secret key to sign tokens
+        'JWT_SIGN_SECRET' : 'cnzkejlze456r4zzre5r4ze4rze4rz6e4r'
+    },
 
-environments.production = {
-    'api_port': 5000,
-    'envName': 'production',
+    "production" : {
+        'api_port': 4000,
+        'envName': 'production',
 
-    // database env
-    "username": "admin",
-    "password": "admin",
-    "database": "sgim_db_prod",
-    "host": "sgim_db",
-    "dialect": "postgres",
+        // database env
+        "username": "admin",
+        "password": "admin",
+        "database": "sgim_db_prod",
+        "host": "sgim_db",
+        "dialect": "postgres",
 
-    // jws secret key to sign tokens
-    'JWT_SIGN_SECRET' : 'cnzkejlze456r4zzre5r4ze4rze4rz6e4r'
-};
+        // jws secret key to sign tokens
+        'JWT_SIGN_SECRET' : 'cnzkejlze456r4zzre5r4ze4rze4rz6e4r'
+    },
 
-environments.test = {
-    'api_port': 4000,
-    'envName': 'test',
+    "test" : {
+        'api_port': 5000,
+        'envName': 'test',
 
-    // database env
-    "username": "admin",
-    "password": "admin",
-    "database": "sgim_db_test",
-    "host": "sgim_db",
-    "dialect": "postgres",
+        // database env
+        "username": "admin",
+        "password": "admin",
+        "database": "sgim_db_test",
+        "host": "sgim_db",
+        "dialect": "postgres",
 
-    // jws secret key to sign tokens
-    'JWT_SIGN_SECRET' : 'cnzkejlze456r4zzre5r4ze4rze4rz6e4r'
-};
-
-var currEnvironment = typeof (process.env.NODE_ENV) == 'string' ? process.env.NODE_ENV : 'development';
-
-// Export the environment
-var environmentToExport = typeof (environments[currEnvironment]) == 'object' ? environments[currEnvironment] : environments.development;
-
-// Export environments module
-module.exports = environmentToExport;
+        // jws secret key to sign tokens
+        'JWT_SIGN_SECRET' : 'cnzkejlze456r4zzre5r4ze4rze4rz6e4r'
+    }
+}
