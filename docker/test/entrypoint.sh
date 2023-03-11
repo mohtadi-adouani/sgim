7c896e6d9af3df925c3669c1115972267bdb04d6
Mohtadi Adouani <madouani@sol-solution.com>
2023-03-11 17:03:45 +0100

    add seeds

--- docker/test/entrypoint.sh
+++ docker/test/entrypoint.sh
@@ -3,7 +3,7 @@ export NODE_ENV="test"
 npx sequelize-cli db:migrate --env test
 echo "Migration __________OK"
 npx sequelize-cli db:seed:undo:all --env test
-#npx sequelize-cli db:seed:all --env production
+npx sequelize-cli db:seed:all --env test
 echo "Seed __________OK"
 npm run start:test
 
